cbad0800e16a89ec62b1816b61acac3173e7e627
pharra <940530348@qq.com>
2017-08-05 03:25:40 -0700

    update

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,77 +1,11 @@
 #!/bin/bash
 
+export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+mkdir out
 export ARCH=arm64
 export SEC_BUILD_OPTION_HW_REVISION=02
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android hero2qlte_chn_open_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android
 
-ROOT_DIR=$(pwd)
-OUT_DIR=$ROOT_DIR/out
-BUILDING_DIR=$OUT_DIR/kernel_obj
-
-JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
-
-#CROSS_COMPILER=$ROOT_DIR/lazy-prebuilt/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-CROSS_COMPILER=$ROOT_DIR/lazy-prebuilt/aarch64-linaro-4.9/bin/aarch64-linux-gnu-
-
-DTBTOOL=$ROOT_DIR/lazy-prebuilt/bin/dtbTool
-DTS_DIR=$BUILDING_DIR/arch/arm64/boot/dts/samsung
-
-ANYKERNEL_DIR=$ROOT_DIR/anykernel-prebuilt
-TEMP_DIR=$OUT_DIR/temp
-
-DEFCONFIG=$1/hero2qlte_chn_open_defconfig
-
-FUNC_PRINT()
-{
-		echo ""
-		echo "=============================================="
-		echo $1
-		echo "=============================================="
-		echo ""
-}
-
-FUNC_CLEAN()
-{
-		FUNC_PRINT "Cleaning All"
-		rm -rf $OUT_DIR
-		mkdir $OUT_DIR
-		mkdir -p $BUILDING_DIR
-		mkdir -p $TEMP_DIR
-}
-
-FUNC_COMPILE_KERNEL()
-{
-		FUNC_PRINT "Start Compiling Kernel"
-		make -C $ROOT_DIR O=$BUILDING_DIR $DEFCONFIG 
-		make -C $ROOT_DIR O=$BUILDING_DIR -j$JOB_NUMBER ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILER
-		FUNC_PRINT "Finish Compiling Kernel"
-}
-
-FUNC_BUILD_DTB()
-{
-		FUNC_PRINT "Building DTB"
-		$DTBTOOL -v -s 2048 -o $TEMP_DIR/dtb $DTS_DIR/
-}
-
-FUNC_PACK()
-{
-		FUNC_PRINT "Start Packing"
-		cp -r $ANYKERNEL_DIR/* $TEMP_DIR
-		cp $BUILDING_DIR/arch/arm64/boot/Image.gz $TEMP_DIR/zImage
-		mkdir $TEMP_DIR/modules
-		find . -type f -name "*.ko" | xargs cp -t $TEMP_DIR/modules
-		cd $TEMP_DIR
-		zip -r HKernel.zip ./*
-		mv HKernel.zip $OUT_DIR/HKernel.zip
-		cd $ROOT_DIR
-		FUNC_PRINT "Finish Packing"
-}
-
-START_TIME=`date +%s`
-FUNC_CLEAN
-FUNC_COMPILE_KERNEL
-FUNC_BUILD_DTB
-FUNC_PACK
-END_TIME=`date +%s`
-
-let "ELAPSED_TIME=$END_TIME-$START_TIME"
-echo "Total compile time is $ELAPSED_TIME seconds"
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7149,7 +7149,6 @@ perf_event_alloc(struct perf_event_attr *attr, int cpu,
 	if (!group_leader)
 		group_leader = event;
 
-	mutex_init(&event->group_leader_mutex);
 	mutex_init(&event->child_mutex);
 	INIT_LIST_HEAD(&event->child_list);
 
@@ -7435,37 +7434,6 @@ static void mutex_lock_double(struct mutex *a, struct mutex *b)
 	mutex_lock_nested(b, SINGLE_DEPTH_NESTING);
 }
 
-/*
- * Variation on perf_event_ctx_lock_nested(), except we take two context
- * mutexes.
- */
-static struct perf_event_context *
-__perf_event_ctx_lock_double(struct perf_event *group_leader,
-			     struct perf_event_context *ctx)
-{
-	struct perf_event_context *gctx;
-
-again:
-	rcu_read_lock();
-	gctx = ACCESS_ONCE(group_leader->ctx);
-	if (!atomic_inc_not_zero(&gctx->refcount)) {
-		rcu_read_unlock();
-		goto again;
-	}
-	rcu_read_unlock();
-
-	mutex_lock_double(&gctx->mutex, &ctx->mutex);
-
-	if (group_leader->ctx != gctx) {
-		mutex_unlock(&ctx->mutex);
-		mutex_unlock(&gctx->mutex);
-		put_ctx(gctx);
-		goto again;
-	}
-
-	return gctx;
-}
-
 /**
  * sys_perf_event_open - open a performance event, associate it to a task/cpu
  *
@@ -7544,16 +7512,6 @@ SYSCALL_DEFINE5(perf_event_open,
 		if (flags & PERF_FLAG_FD_NO_GROUP)
 			group_leader = NULL;
 	}
-
-	/*
-	 * Take the group_leader's group_leader_mutex before observing
-	 * anything in the group leader that leads to changes in ctx,
-	 * many of which may be changing on another thread.
-	 * In particular, we want to take this lock before deciding
-	 * whether we need to move_group.
-	 */
-	if (group_leader)
-		mutex_lock(&group_leader->group_leader_mutex);
 
 	if (pid != -1 && !(flags & PERF_FLAG_PID_CGROUP)) {
 		task = find_lively_task_by_vpid(pid);
@@ -7695,31 +7653,13 @@ SYSCALL_DEFINE5(perf_event_open,
 	}
 
 	if (move_group) {
-		gctx = __perf_event_ctx_lock_double(group_leader, ctx);
-
-		/*
-		 * Check if we raced against another sys_perf_event_open() call
-		 * moving the software group underneath us.
-		 */
-		if (!(group_leader->group_flags & PERF_GROUP_SOFTWARE)) {
-			/*
-			 * If someone moved the group out from under us, check
-			 * if this new event wound up on the same ctx, if so
-			 * its the regular !move_group case, otherwise fail.
-			 */
-			if (gctx != ctx) {
-				err = -EINVAL;
-				goto err_locked;
-			} else {
-				perf_event_ctx_unlock(group_leader, gctx);
-				move_group = 0;
-			}
-		}
+		gctx = group_leader->ctx;
 
 		/*
 		 * See perf_event_ctx_lock() for comments on the details
 		 * of swizzling perf_event::ctx.
 		 */
+		mutex_lock_double(&gctx->mutex, &ctx->mutex);
 		perf_remove_from_context(group_leader, false);
 
 		/*
@@ -7760,12 +7700,10 @@ SYSCALL_DEFINE5(perf_event_open,
 	perf_unpin_context(ctx);
 
 	if (move_group) {
-		perf_event_ctx_unlock(group_leader, gctx);
+		mutex_unlock(&gctx->mutex);
 		put_ctx(gctx);
 	}
 	mutex_unlock(&ctx->mutex);
-	if (group_leader)
-		mutex_unlock(&group_leader->group_leader_mutex);
 
 	put_online_cpus();
 
@@ -7791,11 +7729,6 @@ SYSCALL_DEFINE5(perf_event_open,
 	fd_install(event_fd, event_file);
 	return event_fd;
 
-err_locked:
-	if (move_group)
-		perf_event_ctx_unlock(group_leader, gctx);
-	mutex_unlock(&ctx->mutex);
-	fput(event_file);
 err_context:
 	perf_unpin_context(ctx);
 	put_ctx(ctx);
@@ -7807,8 +7740,6 @@ err_task:
 	if (task)
 		put_task_struct(task);
 err_group_fd:
-	if (group_leader)
-		mutex_unlock(&group_leader->group_leader_mutex);
 	fdput(group);
 err_fd:
 	put_unused_fd(event_fd);
